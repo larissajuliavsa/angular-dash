d91725b3c321349c921baf5e122579b5b8ebf240
larissajuliavsa <larissa.jvsa@gmail.com>
2024-01-22 17:47:28 -0300

    feat: add reactiveforms to register component

--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -8,7 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -28,6 +28,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     FormsModule,
     MatIconModule,
     MatTooltipModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
--- src/app/features/product/register/register.component.ts
+++ src/app/features/product/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../models/product.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -16,48 +17,57 @@ export class RegisterComponent implements OnInit {
   isNewProduct: boolean = false;
   pageTitle: string = '';
 
-  name: string = '';
-  description: string = '';
-  price: string = '';  
-  stock: number = 0;
-  
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private router: Router) {}
+  formRegisterProduct!: FormGroup;
+
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.route = this.activatedRoute.snapshot.url[0].path
+
+    this.createForm();
     
     if(this.route === 'edit-product') {
       this.id = this.activatedRoute.snapshot.url[1].path
       this.productService.getProductById(this.id).subscribe((product: Product) => {
         this.product = product
-        this.name = this.product.name      
-        this.description = this.product.description
-        this.price = this.product.price
-        this.stock = this.product.stock
         this.pageTitle = 'Edit Product'
+        this.formRegisterProduct.controls['name'].setValue(this.product.name)
+        this.formRegisterProduct.controls['description'].setValue(this.product.description)        
+        this.formRegisterProduct.controls['price'].setValue(this.product.price)
+        this.formRegisterProduct.controls['stock'].setValue(this.product.stock)
       })      
-    }
+    } else {
+      this.isNewProduct = true;
+      this.pageTitle = 'New Product'
+    }    
+  }
 
-    this.isNewProduct = true;
-    this.pageTitle = 'New Product'
-    
+  createForm() {
+    this.formRegisterProduct = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      stock: [0, Validators.required],
+    })
   }
 
   saveProduct() {
     const productToSave: Product = {
       id: this.id,
-      name: this.name,
-      price: this.price,
-      description: this.description,
-      stock: this.stock
-    }
-    
-    if (this.isNewProduct) {
-      this.createProduct(productToSave)
+      name: this.formRegisterProduct.controls['name'].value,
+      description: this.formRegisterProduct.controls['description'].value,
+      price: this.formRegisterProduct.controls['price'].value,
+      stock: this.formRegisterProduct.controls['stock'].value,
     }
 
-    productToSave.imageUrl = this.product.imageUrl
-    this.updateProduct(productToSave)
+    console.log(this.formRegisterProduct.getRawValue())
+
+    if (this.isNewProduct) {
+      this.createProduct(productToSave)
+    } else {
+      productToSave.imageUrl = this.product.imageUrl
+      this.updateProduct(productToSave)
+    }
   }
 
   updateProduct(productToSave: Product) {
