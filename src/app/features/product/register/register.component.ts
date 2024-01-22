562d4ede99a5416990b298f0992b10b85d48543f
larissajuliavsa <larissa.jvsa@gmail.com>
2024-01-22 00:08:16 -0300

    feat: register product components

--- src/app/features/product/register/register.component.ts
+++ src/app/features/product/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../models/product.model';
 
 @Component({
@@ -12,12 +12,63 @@ export class RegisterComponent implements OnInit {
 
   id!: string;
   product!: Product;
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {}
+  route: string = '';
+  isNewProduct: boolean = false;
+  pageTitle: string = '';
+
+  name: string = '';
+  description: string = '';
+  price: string = '';  
+  stock: number = 0;
+  
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.id = this.activatedRoute.snapshot.url[1].path
-    this.productService.getProductById(this.id).subscribe((product: Product) => {
-      this.product = product
+    this.route = this.activatedRoute.snapshot.url[0].path
+    
+    if(this.route === 'edit-product') {
+      this.id = this.activatedRoute.snapshot.url[1].path
+      this.productService.getProductById(this.id).subscribe((product: Product) => {
+        this.product = product
+        this.name = this.product.name      
+        this.description = this.product.description
+        this.price = this.product.price
+        this.stock = this.product.stock
+        this.pageTitle = 'Edit Product'
+      })      
+    }
+
+    this.isNewProduct = true;
+    this.pageTitle = 'New Product'
+    
+  }
+
+  saveProduct() {
+    const productToSave: Product = {
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      description: this.description,
+      stock: this.stock
+    }
+    
+    if (this.isNewProduct) {
+      this.createProduct(productToSave)
+    }
+
+    productToSave.imageUrl = this.product.imageUrl
+    this.updateProduct(productToSave)
+  }
+
+  updateProduct(productToSave: Product) {
+    this.productService.updateProduct(productToSave).subscribe(response => {
+      this.router.navigate(['product', 'list'])
+    })
+  }
+
+  createProduct(productToSave: Product) {
+    this.productService.createProduct(productToSave).subscribe(response => {
+      this.router.navigate(['product', 'list'])
     })
   }
 }
